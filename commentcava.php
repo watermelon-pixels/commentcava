482e4731ed55679294303ece64bbc0ebb12bf8a4
fabienwang <fabienwang@free.fr>
2015-03-06 14:00:00 +0100

    started error handling implementation

--- commentcava.php
+++ commentcava.php
@@ -12,12 +12,21 @@ $commentcava = new commentcava($db);
 //Get comments
 if (isset($_GET['a']) && ($_GET['a'] == 'g') and isset($_GET['url'])) {
 
-  header('Cache-Control: no-cache, must-revalidate');
+  $result = ['return_code' => 4, 'entries' => [] ];
+  
+  $result['entries'] = $commentcava->getComments($_GET['url']);
+  
+  if (count($result['entries']))
+  {
+    $result['return_code'] = 0;
+  }
+  
+  /*header('Cache-Control: no-cache, must-revalidate');
   header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
   header('Content-type: application/json');
   header("HTTP/1.1 200 OK");
-
-  $result = $commentcava->getComments($_GET['url']);
+  */
+  
   echo json_encode($result);
 }
 
